e8f66aabde6818752ba13c7d55d2e471f8deed6d
Irvin Pelcaztre-Ortega <ipelcaztreortega@Irvins-Air.wifi.gsu.edu>
2024-02-19 16:47:54 -0500

    Added Components folder, added navbar and styling for it. Removed some basic stuff

---
+++ src/Components/Navbar.jsx
@@ -0,0 +1,25 @@
+import react from 'react'
+import "./Navbar.css"
+
+
+function Navbar(){
+
+
+    return(
+        <div className="Navbar">
+
+            <h1 className="navbar-item R-logo">Rankify</h1>
+
+            <div className="nav-container">
+                <h1 className="navbar-item">Hello</h1>
+                <h1 className="navbar-item">About</h1>
+                <h1 className="navbar-item">Login</h1>
+
+            </div>
+            
+            
+        </div>
+    )
+}
+
+export default Navbar
