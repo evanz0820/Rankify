eceae1770e718d12cff628e346720bf845a8d126
Irvin Pelcaztre-Ortega <ipelcaztreortega@Irvins-Air.wifi.gsu.edu>
2024-02-28 13:44:43 -0500

    Added styling to App.jsx: Search Bar, then created content for welcoming, and then a mock account creation.

--- src/App.jsx
+++ src/App.jsx
@@ -17,15 +17,41 @@ function App() {
 
       {/* This is Irvin's Branch */}
 
+      {/* Search Bar */}
       <div className='search-bar'>
-        <input
+        <input 
+          class="input-bar"
           type='text'
           placeholder='Search...'
           // value={searchTerm}
           // onChange={handleInputChange}
         />
         {/* <img src={searchIcon} alt='Search' className='search-icon' /> */}
+
       </div>
+
+      {/* Second half  */}
+      <div class="content-login-container">
+
+        <div class="inside-content-login">
+
+
+          <div class="left-content">
+            <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Quisquam labore, quos accusamus ea, dolorem provident, quia ducimus delectus beatae aliquam hic. Quis expedita culpa voluptates facilis amet dolorum possimus labore!</p>
+          </div>
+
+            {/* Mock create account bar */}
+          <div class="right-content">
+            <p>Create an Account!</p>
+            <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Nobis, eum? Illo, libero cumque consectetur sit nisi sapiente doloribus possimus blanditiis dolorem consequuntur sed odio iste, recusandae perspiciatis repellat nulla ipsa?</p>
+          </div>
+        
+        </div>
+      </div>
+
+
+
+    {/* Ending div for root */}
     </div>
   )
 }
