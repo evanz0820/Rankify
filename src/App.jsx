4a6e831f34a71f1d455aaf3d4f8f428bbd990e15
Evan Zhang <zhangevan82@gmail.com>
2024-03-20 16:29:20 -0400

    google autocomplete
    Got the autocomplete to work for the search bar.

--- src/App.jsx
+++ src/App.jsx
@@ -1,76 +1,74 @@
-import React, { useState, useEffect } from "react";
+import React, { useState, useEffect, useRef } from "react";
 import './App.css'
 import Navbar from './Components/Navbar'
 import fetchData from './constants/Api';
 import Search from  "./Components/Search"
 import { Link } from 'react-router-dom';
+import Searchbar from "./Components/Searchbar";
+import places from "./Components/places";
+import MapView from './Components/Map'
 
 function App() {
-  const [burgers, setBurgers] = useState([]);
+  const inputRef = useRef()
+  const inputStyle= {
+    boxShadow: 'inset 0 0 10px #eee !important',
+    border: '2px solid #eee',
+    width: '456px',
+    height: '40px',
+    marginLeft: '16px',
+    borderRadius: '20px',
+    fontWeight: '300 !important',
+    outline: 'none',
+    padding: '10px 20px',
+    marginBottom: '10px',
+  }
 
-  useEffect(() => {
-    const fetchDataFromApi = async () => {
-      try {
-        const data = await fetchData();
-        console.log("Fetched data:", data); // Log fetched data
-        if (data && Array.isArray(data)) {
-          setBurgers(data); // Update state with fetched data if it's an array
-        } else {
-          console.error("Data is not an array:", data);
-        }
-      } catch (error) {
-        console.error("Error fetching data:", error);
-      }
-    };
+  const autoComplete = new window.google.maps.places.Autocomplete(
+    inputRef.current,
+  )
 
-    fetchDataFromApi();
-  }, []); // Empty dependency array to run once on mount
-
-  
-
-
+  autoComplete.addListener('place_changed', () => {
+    const place = autoComplete.getPlace()
+    if (!place.geometry || !place.geometry.location) {
+      // User entered the name of a Place that was not suggested and
+      // pressed the Enter key, or the Place Details request failed.
+        alert("this location not available")
+    }
+    if (place.geometry.viewport || place.geometry.location) {
+        // do something
+        console.log(place.geometry.location)
+    }
+  })
   return (
     <div className='App'>
+      
       <Navbar />
 
-      {/* This is Irvin's Branch */}
-
-      {/* Search Bar */}
-      <div className='search-bar'>
-        <input 
-          class="input-bar"
-          type='text'
-          placeholder='Search...'
-          // value={searchTerm}
-          // onChange={handleInputChange}
-        />
-        {/* <img src={searchIcon} alt='Search' className='search-icon' /> */}
-        <Link to="/search"><button>Search!</button></Link>
-      </div>
+      <Searchbar />
 
       {/* Second half  */}
-      <div class="content-login-container">
+      <div className="content-login-container">
 
-        <div class="inside-content-login">
+        <div className="inside-content-login">
 
 
-          <div class="left-content">
+          <div className="left-content">
             <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Quisquam labore, quos accusamus ea, dolorem provident, quia ducimus delectus beatae aliquam hic. Quis expedita culpa voluptates facilis amet dolorum possimus labore!</p>
           </div>
 
             {/* Mock create account bar */}
-          <div class="right-content">
+          <div className="right-content">
             <h1>Create an Account!</h1>
             <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Nobis, eum? Illo, libero cumque consectetur sit nisi sapiente doloribus possimus blanditiis dolorem consequuntur sed odio iste, recusandae perspiciatis repellat nulla ipsa?</p>
-            <input 
-              class="input-bar"
+             <input
+               className="input-bar"
               type='text'
               placeholder='Email Address'
               // value={searchTerm}
               // onChange={handleInputChange}
             />
-            <input 
-              class="input-bar"
+             <input
+               className="input-bar"
               type='text'
               placeholder='Password'
               // value={searchTerm}
@@ -80,29 +78,6 @@ function App() {
         
         </div>
       </div>
-
-
-      {/* Content for burgers */}
-      <h1>Burgers</h1>
-      {/* Render fetched data */}
-      <div className="burgers-container">
-        {burgers.map(burger => (
-          <div key={burger.id} className="burger-item">
-            <h2>{burger.name}</h2>
-            <p>{burger.desc}</p>
-            <p>Price: ${burger.price}</p>
-            <p>{burger.veg ? 'Vegetarian' : 'Non-vegetarian'}</p>
-            <h3>Ingredients:</h3>
-            <ul>
-              {burger.ingredients.map((ingredient, index) => (
-                <li key={index}>{ingredient.name}</li>
-              ))}
-            </ul>
-          </div>
-        ))}
-      </div>
-
-
     </div>
   )
 }
