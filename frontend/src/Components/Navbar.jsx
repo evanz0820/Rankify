495cca57320808796fa9a3f76b2d3a1e500388ed
Evan Zhang <zhangevan82@gmail.com>
2024-04-04 12:36:30 -0400

    Revert "moved the entire frontend folder out"
    This reverts commit 0a3437ad11e46353b7a940f8b3180cb9845b79ed.

--- frontend/src/Components/Navbar.jsx
+++ frontend/src/Components/Navbar.jsx
@@ -1,3 +1,4 @@
+<<<<<<< HEAD
 import react from 'react'
 import "./Navbar.css"
 import { Link } from 'react-router-dom';
@@ -24,3 +25,53 @@ function Navbar(){
 }
 
 export default Navbar
+=======
+// import React from 'react';
+// import { Link } from 'react-router-dom';
+
+// function Navbar() {
+//   return (
+//     <div className="bg-red-400 p-4 flex justify-between items-center">
+//       <h1 className="text-black text-2xl">Rankify</h1>
+//       <div className="flex">
+//         <Link to="/" className="text-black mr-4">Home</Link>
+//         <Link to="/about" className="text-black">About</Link>
+//       </div>
+//     </div>
+//   );
+// }
+
+// export default Navbar;
+
+import React, {useState} from 'react';
+import { Link } from 'react-router-dom';
+import Searchbar from './Searchbar'; // Import the Searchbar component
+
+function Navbar({ onPlaceIDChange }) { // Pass onPlaceIDChange as prop
+
+  const [placeID, setPlaceID] = useState(null);
+
+  const handlePlaceIDChange = (newPlaceID) => {
+    setPlaceID(newPlaceID);
+  }
+  return (
+    <div className="bg-red-400 p-4 flex justify-between items-center">
+      <h1 className="text-black text-2xl">Rankify</h1>
+      <div className='flex w-3/5 '>
+        <Searchbar onPlaceIDChange={handlePlaceIDChange} />
+
+        {/* Centering the button */}
+        <Link className="border-2 border-black rounded w-1/5 flex justify-center items-center" to={`/search/${placeID}`}>
+          <button className="">Search!</button>
+        </Link> 
+      </div>
+      <div className="flex justify-center"> {/* Center align the content */}
+        <Link to="/" className="text-black mr-4">Home</Link>
+        <Link to="/about" className="text-black mr-4">About</Link>
+      </div>
+    </div>
+  );
+}
+
+export default Navbar;
+>>>>>>> parent of 0a3437a (moved the entire frontend folder out)
--- frontend/src/main.jsx
+++ frontend/src/main.jsx
@@ -5,6 +5,7 @@ import './index.css'
 import { BrowserRouter, Route, Routes } from "react-router-dom";
 import Login from "./Components/Login.jsx"
 import Search from  "./Components/Search.jsx"
+<<<<<<< HEAD
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
@@ -18,6 +19,26 @@ ReactDOM.createRoot(document.getElementById('root')).render(
 
       </Routes>
     </BrowserRouter>
+=======
+import Signup from "./Components/Signup.jsx"
+import HomeLogin from "./Components/HomeLogin.jsx"
+import About from "./Components/About.jsx"
+ReactDOM.createRoot(document.getElementById('root')).render(
+  <React.StrictMode>
+  
+    <BrowserRouter>
+          <Routes>
+            <Route path="/" element={<App />} />
+            {/* <Route path="/search" element={<Search />} />  */}
+            <Route path="/search/:placeID" element={<Search />} /> {/* Pass placeID as URL parameter */}
+            <Route path="/signup" element={<Signup/>}></Route>
+            <Route path="/homelogin" element={<HomeLogin/>}></Route>
+            <Route path="/login" element={<Login />} /> {/* Add a Route for Login */}
+            <Route path="/about" element={<About />}></Route>
+          </Routes>
+    </BrowserRouter>
+  
+>>>>>>> parent of 0a3437a (moved the entire frontend folder out)
     {/* <App /> */}
   </React.StrictMode>,
 )
