9dbf9f301d706cc38c67d361ea2c239b7f3a2f27
MariamTmane <mtmane1@student.gsu.edu>
2024-04-08 13:16:21 -0400

    About page done

--- src/Components/About.jsx
+++ src/Components/About.jsx
@@ -1,7 +1,6 @@
 import React from "react";
 import Navbar from "./Navbar";
 import Footer from "./Footer";
-import BoxBasic from "./Box";
 import Accordion from "./Accordion";
 import "./About.css"
 
@@ -12,18 +11,18 @@ function About () {
            
         <Navbar/>
         <div className="text-color">
-        <h1> Rankify connects people with great local businesses.</h1>
+        <h1 className="page-title"> Rankify connects<br></br> people with great local<br></br> <b className="bold">businesses. </b></h1>
         </div>
         </div>
         <br></br>
         <div className="color-intro">
-        <h1>
-            this is the about page
+        <h1 className="title-text">
+            Discover Rankify: Learn More About Us
         </h1>
-        <h3>this is some introduction</h3>
+        <p className="p-text"> <b>Embark on a journey with Rankify and experience the power of community-driven insights. Whether you're a consumer seeking reliable reviews or a business eager to engage with your audience, Rankify provides the platform for meaningful connections and informed decisions. Join us in shaping the future of consumer experiences, where every voice is valued, and transparency reigns supreme. Together, let's elevate the way we interact, review, and connect. </b></p>
+        {/* <h3>this is some introduction</h3> */}
         </div>
         <Accordion className="accordion"></Accordion>
-             
         <Footer></Footer>
 
         </div>
